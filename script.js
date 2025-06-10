ba0bc3ae32abfb8f5a92377336bfd111bd242a11
Santrraaa <santra792006@gmail.com>
2025-06-10 10:19:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+  document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thanks for reaching out! I will get back to you soon.');
+  });
+</script>
